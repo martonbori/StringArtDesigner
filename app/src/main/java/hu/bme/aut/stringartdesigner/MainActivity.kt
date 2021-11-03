5e75f124010fec24cb0522232c31a72497b1145e
martonbori <bori.marton@nagyszekely.hu>
2021-11-03 16:50:37 +0100

    initial commit

--- app/src/main/java/hu/bme/aut/stringartdesigner/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/stringartdesigner/MainActivity.kt
@@ -2,6 +2,7 @@ package hu.bme.aut.stringartdesigner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hu.bme.aut.stringartdesigner.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
