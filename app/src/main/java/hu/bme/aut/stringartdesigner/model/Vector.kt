b6b0b59f62c7745ec82d20103d32e343780eb18d
martonbori <bori.marton@nagyszekely.hu>
2021-11-07 18:26:40 +0100

    added Vector class

---
+++ app/src/main/java/hu/bme/aut/stringartdesigner/model/Vector.kt
@@ -0,0 +1,27 @@
+package hu.bme.aut.stringartdesigner.model
+
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+data class Vector (
+    var x: Float = 0F,
+    var y: Float = 0F
+) {
+    constructor(startPos: Position, endPos: Position) : this() {
+        x = endPos.x - startPos.x
+        y = endPos.y - startPos.y
+    }
+
+    fun length() : Float {
+        return sqrt(x.pow(2) + y.pow(2))
+    }
+
+    fun normalize() : Vector {
+        val l = length()
+        return Vector(x/l, y/l)
+    }
+
+    operator fun times(a: Float) : Vector {
+        return Vector(x*a, y*a)
+    }
+}
